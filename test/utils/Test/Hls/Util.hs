c4343193ad2755cad603b3cd2ac5c6dfdba24a85
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2021-01-18 14:01:51 -0600

    Take HLint's suggestion: demand that GHC not inline hlsCommand.

--- test/utils/Test/Hls/Util.hs
+++ test/utils/Test/Hls/Util.hs
@@ -126,14 +126,13 @@ ignoreForGhcVersions vers reason
 logFilePath :: String
 logFilePath = "hls-" ++ show ghcVersion ++ ".log"
 
-{- HLINT ignore hlsCommand "Missing NOINLINE pragma" -}
-
 -- | The command to execute the version of hls for the current compiler.
 --
 -- Both @stack test@ and @cabal new-test@ setup the environment so @hls@ is
 -- on PATH. Cabal seems to respond to @build-tool-depends@ specifically while
 -- stack just puts all project executables on PATH.
 hlsCommand :: String
+{-# NOINLINE hlsCommand #-}
 hlsCommand = unsafePerformIO $ do
   testExe <- fromMaybe "haskell-language-server" <$> lookupEnv "HLS_TEST_EXE"
   pure $ testExe ++ " --lsp -d -j2 -l test-logs/" ++ logFilePath
